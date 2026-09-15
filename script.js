1b229a38af247ac4566d658e15b2fbc3f3f16f8c
BaitedHeart <dominixen@gmail.com>
2026-09-15 21:39:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ console.log("THE VAULT // System online");
 const core = document.getElementById("vaultCore");
 const systemMessage = document.getElementById("systemMessage");
 
+let sequenceRunning = false;
+
 core.addEventListener("click", function () {
-    console.log("CORE SIGNAL DETECTED");
+
+    if (sequenceRunning) return;
+    sequenceRunning = true;
 
     core.classList.add("activated");
     core.textContent = "◆";
+
     systemMessage.textContent = "CORE SIGNAL DETECTED";
-    
+
+    setTimeout(function () {
+        systemMessage.textContent = "SYNCING...";
+    }, 1200);
+
+    setTimeout(function () {
+        systemMessage.textContent = "DIGITAL SIGNATURE FOUND";
+    }, 2500);
+
+    setTimeout(function () {
+        systemMessage.textContent = "LINK ESTABLISHED";
+    }, 3800);
+
     setTimeout(function () {
         core.classList.remove("activated");
-    }, 800);
+        sequenceRunning = false;
+    }, 4600);
+
 });
