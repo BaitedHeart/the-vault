db2c08288cf30e0dca28199fbf2e00ec65bf8d9e
BaitedHeart <dominixen@gmail.com>
2026-09-15 20:49:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("THE VAULT // System online");
+const core = document.getElementById("vaultCore");
+
+core.addEventListener("click", function () {
+  console.log("CORE SIGNAL DETECTED");
+
+  core.textContent = "◆";
+});
