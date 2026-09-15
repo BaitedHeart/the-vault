9e89b98f919a9f5d11c38ef1d312a18cf9ea53a3
BaitedHeart <dominixen@gmail.com>
2026-09-15 21:34:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 console.log("THE VAULT // System online");
 
 const core = document.getElementById("vaultCore");
+const systemMessage = document.getElementById("systemMessage");
 
 core.addEventListener("click", function () {
     console.log("CORE SIGNAL DETECTED");
 
     core.classList.add("activated");
     core.textContent = "◆";
-
+    systemMessage.textContent = "CORE SIGNAL DETECTED";
+    
     setTimeout(function () {
         core.classList.remove("activated");
     }, 800);
