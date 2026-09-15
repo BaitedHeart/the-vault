e3696dc7294f47e82c3796d341ebe8927c50ca77
BaitedHeart <dominixen@gmail.com>
2026-09-15 11:00:18 +0200

    Update script.js
    Add Vault interaction

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+console.log("The Vault is online.");
 
+const vaultTitle = document.querySelector("h1");
+
+vaultTitle.addEventListener("click", function () {
+  vaultTitle.textContent = "VAULT UNLOCKED 🔓";
+});
